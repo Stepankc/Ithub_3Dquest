38708a9ce0993afd91724f37bbbbe506f6034fc5
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-03-01 17:50:05 +0500

    [TECH] Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -45,9 +43,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
