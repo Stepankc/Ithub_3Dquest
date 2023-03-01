18512329b6a3fca4d4c4843903026ad590633d79
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-03-01 17:47:34 +0500

    [TECH] Initial commit

---
+++ src/app/app.js
@@ -0,0 +1,102 @@
+import "./app.module.scss";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+export const App = () => {
+  //canvas
+  const canvas = document.querySelector("canvas.webgl");
+
+  //scene
+  const scene = new THREE.Scene();
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 0.5);
+  pointLight.position.x = 2;
+  pointLight.position.y = 3;
+  pointLight.position.z = 4;
+  scene.add(pointLight);
+
+  //material
+  const material = new THREE.MeshStandardMaterial({ color: "red" });
+  material.roughness = 0.4;
+
+  const materialPlane = new THREE.MeshStandardMaterial();
+  materialPlane.roughness = 0.4;
+
+  //obj
+  const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+  );
+
+  //plane
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), materialPlane);
+  plane.rotation.x = -Math.PI * 0.5;
+  plane.position.y = -0.65;
+
+  scene.add(mesh, plane);
+
+  //sizes
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  window.addEventListener("resize", () => {
+    //update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    //update render
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  //camera
+  const camera = new THREE.PerspectiveCamera(
+    50,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.set(0, 0, 3);
+  camera.lookAt(mesh.position);
+  scene.add(camera);
+
+  //controls
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+
+  //axesHelp
+  const axesHelper = new THREE.AxesHelper(1);
+  scene.add(axesHelper);
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  const clock = new THREE.Clock();
+
+  const rate = () => {
+    //Clock
+    const elapsedTime = clock.getElapsedTime();
+
+    mesh.rotation.x = Math.sin(elapsedTime);
+    mesh.rotation.y = Math.cos(elapsedTime);
+    //update controls
+    controls.update();
+
+    //render
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(rate);
+  };
+
+  rate();
+};
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
+
+const config = {
+  entry: "./src/core.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "0.0.0.0" || "localhost",
+    port: 5555,
+    hot: true,
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
