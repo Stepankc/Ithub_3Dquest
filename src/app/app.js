e29e367d39ee5dd936f3c04ed8a209b7fcbcba4e
i21s692 <i21s692@students.it-college.ru>
2023-03-07 20:37:45 +0500

    [TECH] delete unuseless code

--- src/app/app.js
+++ src/app/app.js
@@ -1,31 +1,10 @@
 import "./app.module.scss";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { scene } from "./scene/scene";
 
 export const App = () => {
   //canvas
   const canvas = document.querySelector("canvas.webgl");
-
-  //material
-  const material = new THREE.MeshStandardMaterial({ color: "red" });
-  material.roughness = 0.4;
-
-  const materialPlane = new THREE.MeshStandardMaterial();
-  materialPlane.roughness = 0.4;
-
-  //obj
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(0.75, 0.75, 0.75),
-    material
-  );
-
-  //plane
-  const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), materialPlane);
-  plane.rotation.x = -Math.PI * 0.5;
-  plane.position.y = -0.65;
-
-  scene.add(mesh, plane);
 
   //sizes
   const sizes = {
@@ -45,47 +24,10 @@ export const App = () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   });
 
-  //camera
-  const camera = new THREE.PerspectiveCamera(
-    50,
-    sizes.width / sizes.height,
-    0.1,
-    100
-  );
-  camera.position.set(0, 0, 3);
-  camera.lookAt(mesh.position);
-  scene.add(camera);
-
-  //controls
-  const controls = new OrbitControls(camera, canvas);
-  controls.enableDamping = true;
-
-  //axesHelp
-  const axesHelper = new THREE.AxesHelper(1);
-  scene.add(axesHelper);
-
   const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
   });
+
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-  const clock = new THREE.Clock();
-
-  const rate = () => {
-    //Clock
-    const elapsedTime = clock.getElapsedTime();
-
-    mesh.rotation.x = Math.sin(elapsedTime);
-    mesh.rotation.y = Math.cos(elapsedTime);
-    //update controls
-    controls.update();
-
-    //render
-    renderer.render(scene, camera);
-
-    window.requestAnimationFrame(rate);
-  };
-
-  rate();
 };
