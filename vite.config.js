107c4de65cb668215d79e57e1e687cb410005142
i21s692 <i21s692@students.it-college.ru>
2023-03-16 09:07:32 +0500

    [INIT] vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+  root: "src/",
+  publicDir: "./Models/",
+  base: "./",
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+};
