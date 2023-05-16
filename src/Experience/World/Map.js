9becbf4ad07db617edd63b7a33357f2be1ab909e
i21s692 <i21s692@students.it-college.ru>
2023-05-16 06:55:22 +0500

    [FEAT] add dynamic object

--- src/Experience/World/Map.js
+++ src/Experience/World/Map.js
@@ -5,11 +5,13 @@ export default class Map {
   experience = new Experience();
   scene = this.experience.scene;
   zones = this.experience.world.zones;
+  objects = this.experience.world.objects;
   resources = this.experience.resources;
-  
+
   constructor() {
     this.container = new THREE.Object3D();
     this.container.matrixAutoUpdate = false;
+    this.setDynamic();
     this.setTriggerMesh();
     this.setZones();
     this.setMainMap();
@@ -19,7 +21,7 @@ export default class Map {
     console.log(this.store);
     this.scene.add(this.store);
     this.store.scale.set(0.55, 0.25, 0.55);
-    this.store.position.set(-1,-10,0)
+    this.store.position.set(-1, -10, 0);
   }
   setTriggerMesh() {
     //test mesh
@@ -34,12 +36,21 @@ export default class Map {
     this.scene.add(this.testMesh, this.testMesh2);
     this.testMesh2.position.x = 6;
     this.testMesh.name = "mesh1";
-    this.testMesh.userData.drag = "draggable"
-    this.testMesh2.userData.drag = "draggable"
+    this.testMesh.userData.drag = "draggable";
+    this.testMesh2.userData.drag = "draggable";
     this.testMesh2.name = "mesh2";
   }
   setZones() {
     this.zones.add({ x: 0, y: 0 }, { x: 4, y: 4 }, this.testMesh);
     this.zones.add({ x: 10, y: 0 }, { x: 4, y: 5 }, this.testMesh2);
+  }
+  setDynamic() {
+    this.objects.add({
+      base: this.resources.items.aText.scene,
+      collision: this.resources.items.aCollision.scene,
+      offset: new THREE.Vector3(0, 5, 0),
+      rotation: new THREE.Euler(0, 0, -0.2),
+      mass: 0.7,
+    });
   }
 }
