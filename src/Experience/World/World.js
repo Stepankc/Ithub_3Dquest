2c3d30901605d153cd454f12931e137a4fd72747
i21s692 <i21s692@students.it-college.ru>
2023-05-16 05:48:29 +0500

    [REFACTOR] delete fox

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -2,12 +2,9 @@ import * as THREE from "three";
 import Experience from "../Experience.js";
 import Floor from "./Floor.js";
 import Map from "./Map.js";
-import Controls from "./Controls.js";
 import Environment from "./Environment.js";
-import Fox from "./Fox.js";
 import Raycaster from "./Raycaster.js";
 import Zones from "./Zones/Zones.js";
-import Physics from "./Physics.js";
 
 export default class World {
   constructor() {
@@ -16,7 +13,6 @@ export default class World {
     this.resources = this.experience.resources;
     this.camera = this.experience.camera;
     this.renderer = this.experience.renderer;
-    this.controls = new Controls();
 
     // test meshes
     const testMesh = new THREE.Mesh(
@@ -37,43 +33,40 @@ export default class World {
       testMesh.position.z = -Math.random() * 5;
       this.scene.add(testMesh);
     }
+
     this.resources.on("ready", () => {
       //Setup
       this.raycaster = new Raycaster();
       this.createWorld();
       console.log("resources are ready");
-
     });
   }
+
   createWorld() {
     this.setFloor();
-    this.setFox();
     this.setZones();
     this.setEnvironment();
     this.setMap();
-    this.setPhysics();
   }
+
   setFloor() {
     this.floor = new Floor();
   }
-  setFox() {
-    this.fox = new Fox();
-    this.scene.add(this.fox.container)
-  }
+
   setZones() {
     this.zones = new Zones();
     this.scene.add(this.zones.container);
   }
+
   setEnvironment() {
     this.environment = new Environment();
   }
+
   setMap() {
     this.map = new Map();
     this.scene.add(this.map.container);
   }
-  setPhysics() {
-    this.physics = new Physics();
-  }
+
   update() {
     if (this.fox) this.fox.update();
   }
